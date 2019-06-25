c55b478c0fecce5d397dcae524e1e4c8d3c9d909
Anton <34330466+nnton3@users.noreply.github.com>
2019-06-25 19:00:15 +0300

    ui look at camera

---
+++ Simple_Project/Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour
+{
+    Camera _camera;
+
+    private void Start()
+    {
+        _camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        transform.rotation = _camera.transform.rotation;
+    }
+}
